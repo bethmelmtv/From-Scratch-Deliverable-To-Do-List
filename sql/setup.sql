f5a26cd1fe891572511394c1e19ba7f89b1b4ec9
Beth <bethmelmtv@gmail.com>
2022-06-28 16:47:24 -0700

    created user and list table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS list;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE list (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  completed BOOLEAN,
+)
+
+CREATE TABLE users (
+  id BIGING GENERAETD ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+);
