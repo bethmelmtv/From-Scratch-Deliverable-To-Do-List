b25c45438af48a1d36c7496afc38251807b28c3a
Beth <bethmelmtv@gmail.com>
2022-07-01 16:19:03 -0700

    added test to sign in user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
+const UserService = require('../lib/services/UserService');
 const app = require('../lib/app');
 
 //lets create a test user!
@@ -9,6 +10,18 @@ const mockUser = {
   lastName: 'User',
   email: 'rebekah@user.com',
   password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('backend-express-template routes', () => {
@@ -20,11 +33,10 @@ describe('backend-express-template routes', () => {
   it('creates a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
     const { firstName, lastName, email } = mockUser;
-
     expect(res.body).toEqual({
       id: expect.any(String),
       firstName,
-      `lastName`,
+      lastName,
       email,
     });
   });
